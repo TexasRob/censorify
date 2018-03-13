c7d53f72e8c0bbf2fafea7b733554f3e871c7f2a
Rob Gura <rgura@rgura-ltm.internal.salesforce.com>
2018-03-13 17:12:47 -0500

    Initial release

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a sample Node.js module, I created it as part of working through "Node.js, MongoDB and Angular Web Development, Second Edition.
---
+++ censortext.js
@@ -0,0 +1,22 @@
+var censoredWords = ["sad", "bad", "mad"];
+var customCensoredWords =[];
+
+function censor(inStr) {
+	for (idx in censoredWords) {
+		inStr = inStr.replace(censoredWords[idx], "****");
+	}
+	for (idx in customCensoredWord) {
+		inStr = inStr.replace(customCensoredWord[idx], "****");
+	}
+	return inStr;
+
+}
+function addCensoredWords(word) {
+	customCensoredWord.push(word);
+}
+function getCensoredWords () {
+	return censoredWords.concat(customCensoredWords);
+}
+exports.censor = censor;
+exports.addCensoredWords = addCensoredWords;
+exports.getCensoredWords = getCensoredWords;
